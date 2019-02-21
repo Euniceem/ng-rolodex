c04a72fd315e5dff8f2929ffdb0cc7d45d73f636
Eunice Min <euniceem1007@gmail.com>
2019-02-21 12:28:38 -1000

    changed file path

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './login/login.component.html',
-  styleUrls: ['./login/login.component.scss']
+  templateUrl: '../login/login.component.html',
+  styleUrls: ['../login/login.component.scss']
 })
 
 export class LoginComponent {
