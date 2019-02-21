d86af3d418d588f8f265f14bf64a4b2346ceceb7
Eunice Min <euniceem1007@gmail.com>
2019-02-21 12:28:12 -1000

    created paths for anchor and button tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app/app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app/app.component';
 
 //Page Components
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AddContactComponent } from './pages/add-contact/addContact.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
 //Shared Components
@@ -22,6 +23,7 @@ import { HeaderComponent } from '../shared/header/header.component';
     HomeComponent,
     LoginComponent,
     ContactComponent,
+    AddContactComponent,
     ProfileComponent,
   ],
   imports: [
