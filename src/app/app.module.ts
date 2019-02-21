f62e031b6a84ac8c85e8be16cf479c8c4fc6552f
Eunice Min <euniceem1007@gmail.com>
2019-02-20 22:26:34 -1000

    basic component file setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //For input handling
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app.component';
+
+//Page Components
+import { HeaderComponent } from '../shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
