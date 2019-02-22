a31fd8b4297b2974a23f70b87c3f66912fa6f846
Eunice Min <euniceem1007@gmail.com>
2019-02-21 16:50:11 -1000

    creating authentication with angular

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: '../login/login.component.html',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
-  constructor() { }
+  loginFormData: {
+    username: string,
+    password: string
+  } = {
+      username: '',
+      password: ''
+    };
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  login() {
+    this.auth.login(this.loginFormData)
+      .then(() => {
+        console.log('User logged in!');
+        const redirectUrl = this.auth.redirectUrl;
+        if (redirectUrl) {
+          this.router.navigate([redirectUrl])
+          this.auth.redirectUrl = '';
+        } else {
+          this.router.navigate(['/'])
+        }
+      })
+      .catch((err) => {
+        console.log('error:', err);
+      })
+  }
 }
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -7,4 +7,19 @@ import { HttpClient } from '@angular/common/http';
 
 export class BackendService {
   constructor(private http: HttpClient) { }
+
+  register(user) {
+    return Promise.resolve({});
+  }
+
+  login(user) {
+    return Promise.resolve({
+      id: 1,
+      username: user.username
+    })
+  }
+
+  logout() {
+    return Promise.resolve({});
+  }
 }
