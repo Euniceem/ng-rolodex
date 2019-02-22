62eda5d08b1bd0403e253bf3f9154e068e5ea1b4
Eunice Min <euniceem1007@gmail.com>
2019-02-22 10:08:56 -1000

    changed file directory

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: '../contact/contact.component.html',
-  styleUrls: ['../contact/contact.component.scss']
+  templateUrl: '.contact.component.html',
+  styleUrls: ['.contact.component.scss']
 })
 export class ContactComponent {
   constructor() { }
