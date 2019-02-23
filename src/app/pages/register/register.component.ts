04ef897f07cdb18ceee63d1890206700f6f4273c
Eunice Min <euniceem1007@gmail.com>
2019-02-22 22:42:18 -1000

    created register component

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  registerFormData: {
+    username: string,
+    password: string,
+  } = {
+      username: '',
+      password: ''
+    };
+
+  constructor( 
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  register() {
+    this.auth.register(this.registerFormData)
+    .then(()=> {
+      this.router.navigate(['/login']);
+    })
+    .catch((err) => { 
+      this.router.navigate(['/register']);
+    });
+  }
+}
