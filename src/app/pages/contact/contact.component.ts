989849c47e9697724b7afdc231f5f0fb0dd82c00
Eunice Min <euniceem1007@gmail.com>
2019-02-23 15:51:32 -1000

    working on getting values from database

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
-  constructor() { }
+export class ContactComponent implements OnInit {
+
+  contactData: {
+    name: string,
+    address: string,
+    mobile: string,
+    work: string,
+    home: string,
+    email: string,
+    twitter: string,
+    instagram: string,
+    github: string
+  } = {
+      name: '',
+      address: '',
+      mobile: '',
+      work: '',
+      home: '',
+      email: '',
+      twitter: '',
+      instagram: '',
+      github: ''
+    }
+
+  constructor(private backend: BackendService) { }
+
+  ngOnInit() {
+    return this.backend.contacts()
+
+  }
 }
