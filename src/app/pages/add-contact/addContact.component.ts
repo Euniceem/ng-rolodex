a91c3f7bbc4b964aaac5f9ff28ca3ea263a0a00c
Eunice Min <euniceem1007@gmail.com>
2019-02-24 21:19:33 -1000

    added html form and got backend working with addcontact component

--- src/app/pages/add-contact/addContact.component.ts
+++ src/app/pages/add-contact/addContact.component.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   templateUrl: './addContact.component.html',
   styleUrls: ['./addContact.component.scss']
 })
-export class AddContactComponent {
-  constructor() { }
+export class AddContactComponent implements OnInit {
+
+  addContactDataForm: {
+    name: string,
+    address: string,
+    mobile: string,
+    work: string,
+    home: string,
+    email: string,
+    twitter: string,
+    instagram: string,
+    github: string
+  } = {
+      name: '',
+      address: '',
+      mobile: '',
+      work: '',
+      home: '',
+      email: '',
+      twitter: '',
+      instagram: '',
+      github: ''
+    }
+
+  isEmailInValid: boolean = true;
+  invalidAddContact: boolean = false;
+
+  constructor(
+    private backend: BackendService,
+    private router: Router
+  ) { }
+
+  validateEmail() {
+    const { email } = this.addContactDataForm;
+
+    if (!email) { this.isEmailInValid = true; }
+    else if (email.includes('@')) { this.isEmailInValid = true; }
+    else { this.isEmailInValid = false; }
+  }
+
+  ngOnInit() {
+    return this.backend.contacts()
+      .then((data) => {
+        for (var key in data) {
+          this.addContactDataForm[key] = data[key];
+        }
+      })
+  }
+
+  submitForm() {
+    this.backend.addContact(this.addContactDataForm)
+      .then(() => {
+        this.router.navigate(['/contacts']);
+      })
+      .catch((err) => {
+        this.invalidAddContact = true;
+      })
+  }
+
 }
